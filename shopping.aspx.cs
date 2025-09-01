f75ca15b0706902f760ad244572550f622f8c087
VishalMer <vishalmer99747@gmail.com>
2025-09-01 23:08:59 +0530

    refactor: implemented proper styling and database connection for the shopping page.

---
+++ shopping.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace JenStore
+{
+    public partial class shopping1 : System.Web.UI.Page
+    {
+        string connect = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindProductRepeater();
+            }
+        }
+
+        private void BindProductRepeater()
+        {
+            using (SqlConnection con = new SqlConnection(connect))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Products", con))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+                        rptProducts.DataSource = dt;
+                        rptProducts.DataBind();
+                    }
+                }
+            }
+        }
+
+    }
+}
